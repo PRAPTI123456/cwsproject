3f041cf2657a93c738619f6fac3556f9745a44f6
kartiklende <165433930+kartiklende@users.noreply.github.com>
2024-04-20 01:27:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+import './App.css';
+import SearchComponent from './components/SearchComponent';
+import ShowCourseComponent from './components/ShowCourseComponent';
+import UserCartComponent from './components/UserCartComponent';
+
+function App() {
+	const [courses, setCourses] = useState([
+		{ id: 1, 
+		name: 'One Piece', 
+		price: 899, 
+		image: 
+'https://i.imgur.com/tz2YP98.png'
+		},
+		{ id: 2, 
+		name: 'Jeans', 
+		price: 699, 
+		image: 
+'https://i.imgur.com/Z1kCkKD.png'
+		},
+		{ id: 3, 
+		name: 'Top', 
+		price: 299, 
+		image: 
+'https://i.imgur.com/268PEOo.png'
+		}
+	]);
+
+	const [cartCourses, setCartCourses] = useState([]);
+	const [searchCourse, setSearchCourse] = useState('');
+
+	const addCourseToCartFunction = (GFGcourse) => {
+		const alreadyCourses = cartCourses
+							.find(item => item.product.id === GFGcourse.id);
+		if (alreadyCourses) {
+			const latestCartUpdate = cartCourses.map(item =>
+				item.product.id === GFGcourse.id ? { 
+				...item, quantity: item.quantity + 1 } 
+				: item
+			);
+			setCartCourses(latestCartUpdate);
+		} else {
+			setCartCourses([...cartCourses, {product: GFGcourse, quantity: 1}]);
+		}
+	};
+
+	const deleteCourseFromCartFunction = (GFGCourse) => {
+		const updatedCart = cartCourses
+							.filter(item => item.product.id !== GFGCourse.id);
+		setCartCourses(updatedCart);
+	};
+
+	const totalAmountCalculationFunction = () => {
+		return cartCourses
+			.reduce((total, item) => 
+						total + item.product.price * item.quantity, 0);
+	};
+
+	const courseSearchUserFunction = (event) => {
+		setSearchCourse(event.target.value);
+	};
+
+	const filterCourseFunction = courses.filter((course) =>
+		course.name.toLowerCase().includes(searchCourse.toLowerCase())
+	);
+
+	return (
+		<div className="App">
+			<SearchComponent searchCourse={searchCourse} 
+							courseSearchUserFunction=
+								{courseSearchUserFunction} />
+			<main className="App-main">
+				<ShowCourseComponent
+					courses={courses}
+					filterCourseFunction={filterCourseFunction}
+					addCourseToCartFunction={addCourseToCartFunction}
+				/>
+
+				<UserCartComponent
+					cartCourses={cartCourses}
+					deleteCourseFromCartFunction={deleteCourseFromCartFunction}
+					totalAmountCalculationFunction={
+						totalAmountCalculationFunction
+					}
+					setCartCourses={setCartCourses}
+				/>
+			</main>
+		</div>
+	);
+}
+
+export default App;
